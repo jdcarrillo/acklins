dd4d4d753f9fe7886eeb2cd05df87b02ed119509
jcm159 <jcm159@gmail.com>
2014-04-07 22:53:48 -0500

    ok

---
+++ contactos/src/contactos/Main.java
@@ -0,0 +1,12 @@
+package contactos;
+/**
+ * @web http://jc-mouse.blogspot.com/
+ * @author Mouse
+ */
+public class Main {
+    
+    public static void main(String[] args) {        
+        tablaform tf = new tablaform();
+        tf.show();                  
+    }
+}
---
+++ contactos/src/contactos/mipanel.java
@@ -0,0 +1,37 @@
+package contactos;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+/**
+ * @web http://jc-mouse.blogspot.com/
+ * @author Mouse
+ */
+public class mipanel extends javax.swing.JPanel {
+    private int b=0;
+    
+    public mipanel(int w, int h){    
+        this.setSize(w,h);
+    }
+    
+   public mipanel(int w, int h, int b) {
+        this.b=b;
+        this.setSize(w,h);
+   }
+   
+    @Override
+    //public void paintComponent(Graphics g){
+    public void paint(Graphics g){
+        //Dimension tamanio = getSize();
+        
+        if (this.b == 0){
+            ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/imagenes/vista.jpg"));                     
+            g.drawImage(imagenFondo.getImage(),0,0,getWidth(),getHeight(), null);                
+        }
+        else
+        {
+            ImageIcon imagenFondo = new ImageIcon(getClass().getResource("/imagenes/mouse_mbanner.jpg"));                                 
+            g.drawImage(imagenFondo.getImage(),0,0,getWidth(),getHeight(), null);                
+        }           
+        setOpaque(false);
+        super.paintComponent(g);
+    }    
+}
